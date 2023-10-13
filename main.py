638f814ab589bbea17d40bc52faaf3a3a5f30602
Patrickpisana23 <patrickpisana@unmsm.edu.pe>
2023-10-13 16:28:31 -0500

    new line

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pandasai.llm.openai import OpenAI
 
 load_dotenv()
 
-st.title("ChatGPT with PandasAI")
+st.title("ChatGPT con PandasAI")
 
 # Agregar un campo de entrada de texto para que el usuario ingrese el API Key
 api_key = st.text_input("Ingrese su API Key de ChatGPT", type="password")
@@ -18,7 +18,7 @@ if api_key:
     llm = OpenAI(api_token=api_key)
     pandas_ai = PandasAI(llm)
 
-    uploaded_file = st.file_uploader("Cargar archivo CSV para el análisis", type=['xlsx'])
+    uploaded_file = st.file_uploader("Cargar archivo Excel para el análisis", type=['xlsx'])
 
     if uploaded_file is not None:
         df = pd.read_excel(uploaded_file)
